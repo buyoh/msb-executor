301a7bc4bdcc52a2b066d4178fb5fa5a29714962
buyoh <japanjetone@live.jp>
2020-05-06 15:25:35 +0900

    wip: manipulator handling

--- src/app_server_session.rb
+++ src/app_server_session.rb
@@ -1,6 +1,12 @@
+require_relative './executor/executor_room.rb'
+require_relative './room_manipulator.rb'
+
 class AppServerSession
+
   def initialize
     @onpost = nil
+    # @room = nil
+    @manipulator = nil
   end
 
   # handler posting to client
@@ -8,47 +14,86 @@ class AppServerSession
     @onpost = pc
   end
 
-  def close
-    
+  #
+
+  def close_room
+    if @manipulator
+      @manipulator.close
+      @manipulator = nil
+    end
   end
+
+  def close
+    close_room
+    # note: discard @onpost ??
+  end
+
+  #
+
+  def error_handling(msg, id)
+    @onpost.call({
+      stat: false, id: id,
+      reason: msg
+    })
+  end
+
+  #
 
   def handle(data)
     act = data['act']
+    data['id'] ||= rand(1e9.to_i).to_s
     if ['ping'].include?(act)
-      return __send__("handle_#{act}", data)
+      __send__("handle_#{act}", data)
     else
-      return {stat: false, reason: 'unknown act'}
+      error_handling 'unknown act', data['id']
     end
   end
 
   # ping (for test)
   def handle_ping(data)
     @onpost.call({
-      stat: true,
+      stat: true, id: data['id']
       date: Time.now.to_i
     })
   end
 
   # room for executor
-  def handle_openroom
+  def handle_openroom(data)
+    if @manipulator
+      error_handling 'illegal states'
+      return
+    end
+    room = ExecutorRoom.new
+    @manipulator = RoomManipulator.new(room)
   end
 
   # room for executor
-  def handle_closeroom
+  def handle_closeroom(data)
+    if @manipulator
+      error_handling 'illegal states'
+      return
+    end
+    close_room
   end
 
   # upload and write files
-  def handle_upload
+  def handle_hogehoge(data)
     
   end
 
   # exec
-  def handle_run
+  def handle_run(data)
     # noblock, async
   end
 
-  def handle_run_send
-    # interactive
+  def handle_run_rich(data)
+    # undefined...
+    # it may be nice method
+  end
+
+  def handle_run_interactive(data)
+    # interactive running mode
+    # perhaps, need unko.
   end
 
 end
---
+++ src/room_manipulator.rb
@@ -0,0 +1,67 @@
+class RoomManipulator
+
+  def initialize(room)
+    # note: *_immとflushだけなので、メンバ変数として保持する理由が無さそう
+    # app_server_sessionに持ってもらう
+    @room = room
+    @queue = []
+    @result = []
+  end
+
+  def closed?
+    @room.nil?
+  end
+
+  def close
+    return nil if closed?
+    @room.destroy
+    @room = nil
+  end
+
+  # push to queue
+  def write(path, string, opt = {})
+    @queue << [:write_q, path, string, opt]
+  end
+
+  # push to queue
+  def run(command)
+    @queue << [:run_q, command, {}]
+  end
+
+  def read_imm(path, length, offset = 0, opt = {})
+    # ファイルが大きいなら圧縮して送った方が良さげ
+    d = nil
+    @room.chdir do
+      d = IO.read(path, length, offset, opt = {})
+    end
+    d
+  end
+
+  def flush
+    @room.chdir do
+      while !@queue.empty?
+        elem = @queue.shift
+        m = elem.shift
+        __call__ m, *elem
+      end
+    end
+  end
+
+  def write_q(path, string, opt)
+    z = IO.write(path, string, opt)
+    @result << z
+  end
+  private :write_q
+
+  def run_q(command, opt)
+    e = Executor.new({
+      cmd: command,
+      stdout: ['stdout.log', 'a'],
+      stderr: ['stderr.log', 'a']
+    }.merge(opt))
+    _, stt = e.execute
+    @result << e.exitstatus
+  end
+  private :run_q
+
+end
